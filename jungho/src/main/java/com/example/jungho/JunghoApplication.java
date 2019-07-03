1621e7518953fb4a794c3285a9ec2084d765d5fd
jpumpkin93 <jpumpkin93@gmail.com>
2019-07-03 19:28:15 +0900

    first commit

--- jungho/src/main/java/com/example/jungho/JunghoApplication.java
+++ jungho/src/main/java/com/example/jungho/JunghoApplication.java
@@ -1,10 +1,16 @@
 package com.example.jungho;
 
+import com.example.jungho.domain.User;
+import com.example.jungho.repo.UserJpaRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class JunghoApplication {
@@ -17,5 +23,4 @@ public class JunghoApplication {
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
-
 }
